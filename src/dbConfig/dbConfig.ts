19c041789cac24bfffe61d2e882b6f7658c4d211
MontuSherasiya <sherasiyamontu@gmail.com>
2026-06-07 20:32:45 +0530

    add user model and database connection setup

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URI!)
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log('Connected to MongoDB');
+        });
+
+        connection.on('error', (err) => {
+            console.error('MongoDB connection error: ', err);
+            process.exit();
+        });
+
+    } catch (error) {
+        console.error('Something went wrong in connecting to DB: ', error);
+    }
+}
