4cee32f29b84e6174acecf37506369ed6c2d0b7a
MontuSherasiya <sherasiyamontu@gmail.com>
2026-06-08 19:44:30 +0530

    add user signup functionality with email verification

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,7 +1,22 @@
 import nodemailer from 'nodemailer';
+import User from '@/models/userModel'
+import bcryptjs from 'bcryptjs';
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
     try {
+
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+
+        if (emailType === 'VERIFY') {
+            await User.findByIdAndUpdate(userId,
+                { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000 }
+            );
+        } else if (emailType === 'RESET') {
+            await User.findByIdAndUpdate(userId,
+                { forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000 }
+            );
+        }
+
         const transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
             port: Number(process.env.EMAIL_PORT),
@@ -16,7 +31,9 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
             from: process.env.EMAIL_FROM,
             to: email,
             subject: emailType === 'VERIFY' ? 'Verify Your Email' : 'Reset Your Password',
-            html: '<b>Hello World</b>',
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            </p>`
         }
 
         const mailResponse = await transporter.sendMail(mailOptions);
